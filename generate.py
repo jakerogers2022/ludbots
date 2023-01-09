bf82bf48d75894bcbd5763303b2f3df807febc39
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2023-01-09 13:24:56 -0600

    first part

--- generate.py
+++ generate.py
@@ -1,25 +1,35 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
 
-length = 1
-width = 1
-height = 1
+    length = 1
+    width = 1
+    height = 1
 
-x = 0
-y = 0
-z = 0.5
+    x = -1
+    y = 1
+    z = 0.5
 
-for i in range (5):
-    y = i
-    for i in range (5):
-        x = i
-        l= 1
-        for i in range(10):
-            nm = "Box"+str(i)
-            pyrosim.Send_Cube(name=nm, pos=[x,y,z+i] , size=[l,l,l])
-            l = 0.9 *l
+    pyrosim.Send_Cube(name="box", pos=[x,y,z] , size=[length,width,height])
 
 
+    pyrosim.End()
 
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length = 1
+    width = 1
+    height = 1
+
+    x = 0
+    y = 0
+    z = 0.5
+
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Leg", pos= [1.0,0,1.5], size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0.5,0.5])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
