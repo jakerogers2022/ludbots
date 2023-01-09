3fce85a66fee2d244676faab2c66900d300e3d7f
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2023-01-09 13:03:15 -0600

    partd

--- generate.py
+++ generate.py
@@ -1,7 +1,25 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = 0.5
+
+for i in range (5):
+    y = i
+    for i in range (5):
+        x = i
+        l= 1
+        for i in range(10):
+            nm = "Box"+str(i)
+            pyrosim.Send_Cube(name=nm, pos=[x,y,z+i] , size=[l,l,l])
+            l = 0.9 *l
+
+
 
 pyrosim.End()
